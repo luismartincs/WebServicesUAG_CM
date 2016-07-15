3ec118c717e50441744995c2f5bd79a8151f1801
Luis de Jesus Martin Castillo <luismartin.925@gmail.com>
2016-07-15 18:39:56 -0500

    Initial Commit

---
+++ WebServices/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebServices
+//
+//  Created by Luis de Jesus Martin Castillo on 15/07/16.
+//  Copyright © 2016 Luis de Jesus Martin Castillo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
